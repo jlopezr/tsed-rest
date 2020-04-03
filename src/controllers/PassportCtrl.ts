084cbb595f8360c02a5a8194154bd43dcbc4946b
Juan López Rubio <j.lopez.r@gmail.com>
2020-04-03 09:02:08 +0200

    use credentials instead of separate bodyparams

--- src/controllers/PassportCtrl.ts
+++ src/controllers/PassportCtrl.ts
@@ -1,15 +1,16 @@
 import {BodyParams, Controller, Post, ProviderScope, Req, Scope, $log} from "@tsed/common";
 import {Authenticate} from "@tsed/passport";
+import { Credentials } from "../models/Credentials";
 
 @Controller("/")
 @Scope(ProviderScope.SINGLETON)
 export class PassportCtrl {
   @Post("/login")
   @Authenticate("login")
-  login(@Req() req: Req, @BodyParams("email") email: string, @BodyParams("password") password: string) {
+  login(@Req() req: Req, @BodyParams() credentials: Credentials) {
     $log.info("LOGIN");
     $log.info(JSON.stringify(req.body));
-    $log.info(`EMAIL ${email} PASS ${password}`);
+    $log.info(`EMAIL ${credentials.email} PASS ${credentials.password}`);
     return req.user;
   }
 }
